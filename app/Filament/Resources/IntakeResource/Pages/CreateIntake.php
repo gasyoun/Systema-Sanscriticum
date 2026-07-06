d61efc4ed680da1a4038159656237769dee84928
Mārcis Gasūns <gasyoun@ya.ru>
2026-07-06 16:23:15 +0300

    ai-wip: watcher-safe autosave CreateIntake.php

---
+++ app/Filament/Resources/IntakeResource/Pages/CreateIntake.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\IntakeResource\Pages;
+
+use App\Filament\Resources\IntakeResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateIntake extends CreateRecord
+{
+    protected static string $resource = IntakeResource::class;
+}
