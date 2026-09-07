fc4af08bb2d50679dbdde274c8da2c56abde3415
Mārcis Gasūns <gasyoun@ya.ru>
2026-09-07 10:40:27 +0300

    fix(surveys): describe depth survey in invitation

--- tests/Feature/SurveyStudentInviteTest.php
+++ tests/Feature/SurveyStudentInviteTest.php
@@ -148,7 +148,7 @@ class SurveyStudentInviteTest extends TestCase
             $text = (string) ($request->data()['text'] ?? '');
 
             return str_contains($text, 'https://samskrte.ru/anketa/'.self::SLUG)
-                && str_contains($text, 'Заполнить опрос')
+                && str_contains($text, 'Заполнить опрос')`r`n                && str_contains($text, '15–20 минут')
                 && ! str_contains($text, 'наград')
                 && ! str_contains($text, 'приз');
         });
