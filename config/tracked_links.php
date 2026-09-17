31c8338133dd0e06afd34b713df9597f38b6adaa
Mārcis Gasūns <gasyoun@ya.ru>
2026-09-17 12:58:48 +0300

    Add tracking for evergreen story series

--- config/tracked_links.php
+++ config/tracked_links.php
@@ -53,6 +53,16 @@ return [
             'utm_campaign' => $campaign,
             'utm_term' => 'beginner',
         ],
+        'lingq' => [
+            'destination' => 'https://t.me/samskrtamru/4166',
+            'utm_campaign' => 'linguistics_tasks_evergreen',
+            'utm_term' => 'puzzle',
+        ],
+        'linga' => [
+            'destination' => 'https://t.me/samskrtamru/4169',
+            'utm_campaign' => 'linguistics_tasks_evergreen',
+            'utm_term' => 'answer',
+        ],
     ],
     'story_accounts' => [
         'mg' => 'telegram_marcisgasuns',
